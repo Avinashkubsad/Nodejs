010b0c1148c1d66e7c57b1a1137cfc9145b19c67
avinash <avi.kubsad14@gmail.com>
2025-08-10 21:05:36 +0530

    mongodb conneted

---
+++ CRUD/index.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const dotenv = require("dotenv");
+
+
+const app = express();
+
+app.use(bodyParser.json());
+dotenv.config();
+
+const PORT = process.env.PORT || 5000;
+const MONGODB_URI = process.env.MONGODB_URI;
+
+mongoose
+  .connect(MONGODB_URI)
+  .then(async () => {
+    console.log("Database connected succesfully");
+
+    // // Check MongoDB databases
+    // const dbs = await mongoose.connection.db.admin().listDatabases();
+    // console.log("📂 Databases:", dbs);
+
+    app.listen(PORT, () => {
+      console.log(`Server is rnning on PORT ${PORT} `);
+    });
+  })
+  .catch((error) => console.log(error));
+
