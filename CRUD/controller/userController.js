02398a29a7221d3a77c3310b3f4a496a2c19831a
avinash <avi.kubsad14@gmail.com>
2025-08-12 20:56:21 +0530

    added validation and console.log

--- CRUD/controller/userController.js
+++ CRUD/controller/userController.js
@@ -7,17 +7,35 @@ export const create = async (req, res) => {
     console.log("data incoming")
     const userData = new User(req.body);
 
-    const { email } = userData;
+    const { email,phno } = userData;
 
-    const userExist = await User.findOne({ email });
-    if (userExist) {
-      return res.status(400).json({ message: "User already exist" });
+      // Validate email
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    if (!emailRegex.test(email)) {
+      return res.status(400).json({ message: "Invalid email format" });
     }
 
+    // Validate phone number (exactly 10 digits)
+    const phoneRegex = /^\d{10}$/;
+    if (!phoneRegex.test(phno)) {
+      return res.status(400).json({ message: "Phone number must be exactly 10 digits" });
+    }                                                                                          //No spaces, no special characters, no country code
+    
+    const userExist = await User.findOne({ email });
+    if (userExist) {
+      console.log("user already exists")
+      return res.status(400).json({ message: "User already exist" }); 
+    }
+    
     const savedUser = await userData.save();
     res.status(200).json(savedUser);
-  } catch {}
+
+    console.log("dATA SAVED SUCCESSFULLY",savedUser);
+  } catch (error){
+    res.status(500).json({error : "Internal server error"})
+  }
 };
+
 
 export const fetch = async (req, res) => {
   try {
